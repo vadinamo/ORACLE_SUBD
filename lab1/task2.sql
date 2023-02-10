1049f19f0f312472dae69cb5c5146f8549f9ae74
vadinamo <vadinamo@gmail.com>
2023-02-10 16:40:46 +0300

    task2

---
+++ lab1/task2.sql
@@ -0,0 +1,10 @@
+DECLARE
+    i NUMBER := 0;
+BEGIN
+    LOOP
+        INSERT INTO MyTable
+            VALUES (i, ROUND(DBMS_RANDOM.VALUE(0, 10000)));
+        i := i + 1;
+        EXIT WHEN i >= 10000;
+    END LOOP;
+END;
